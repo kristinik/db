e74b1e63a90b6c62919a4b5fb5744b3967b5ca47
kristinik <150954793+kristinik@users.noreply.github.com>
2025-09-29 04:24:37 +0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,7 +1,9 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "mysql+pymysql://root:1111@127.0.0.1:3306/your_database1234")
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "DATABASE_URL",
+        "mysql+pymysql://appuser:IlhMKrDeEroXDRrXFMNV@"
+        "appdb.cr6squ8gqocf.eu-central-1.rds.amazonaws.com:3306/your_database1234"
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JSON_SORT_KEYS = False
-    SECRET_KEY = os.environ.get("SECRET_KEY", "change-me")
