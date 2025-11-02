d1cedf683733179294bab012bc38aa62b8431ed7
kristinik <150954793+kristinik@users.noreply.github.com>
2025-11-02 20:53:56 +0200

    Dynamically import app module from app.py
    Load app module dynamically if app.py exists.

--- wsgi.py
+++ wsgi.py
@@ -1,1 +1,13 @@
-from app import app 
+
+import importlib.util, os
+
+here = os.path.dirname(__file__)
+app_py = os.path.join(here, "app.py")
+
+if os.path.isfile(app_py):
+    spec = importlib.util.spec_from_file_location("app_file", app_py)
+    mod = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(mod)
+    app = mod.app          
+else:
+    from app import app
